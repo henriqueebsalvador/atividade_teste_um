9c6b8e0d8edd662254cdb6d756810ff9e37f6262
henrique <henrique_b_salvador@estudante.sesisenai.org.br>
2026-06-02 09:42:24 -0300

    alt - reformulação

--- index.php
+++ index.php
@@ -1,44 +1,55 @@
 <?php
-session_start();
+    session_start();
 
-$host = "localhost";
-$user = "root";
-$pass = "root";
-$db = "sistema_simples7";
+    include("infra/db/connect.php");
 
-$conn = new mysqli($host,$user,$pass,$db);
-if ($conn -> connect_error) {
-    die("Conexão falhou: " . $conn -> connect_error);
-}else{
-    echo "<p> Banco: OK! </p>";
-};
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usuario = $_POST["usuario"];
-    $senha = $_POST["senha"];
-}
+        $usuario = $_POST["usuario"];
+        $senha = $_POST["senha"];
+        
+        $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
+        $resultado = $conn->query($sql);
 
+        if ($resultado->num_rows > 0){
+            $_SESSION["usuario"] = $usuario;
+            header("Location: public/home.php");
+            exit();
+        }else{
+            $erro = "Usuário ou senha inválidos!";
+        }
+    }
 ?>
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login com Banco</title>
+    <title>Login</title>
 </head>
 <body>
-    <h2>
-        Login com PHP
-    </h2>
-        <form method="POST">
-            <label for="usuario">Usuario</label>
-            <input type="text" name="usuario">
-            <br><br>
-            <label for="senha">Senha</label>
-            <input type="password" name="senha">
-            <br><br>
-            <button type="submit">Enviar</button>
-        </form>
+    <h1>Sitema de Login Simples</h1>
+
+    <form method="POST">
+        <label>Usuário:</label>
+        <input type="text" name="usuario">
+        <br>
+        <label>Senha:</label>
+        <input type="password" name="senha">
+        <br>
+        <?php
+        
+            if(isset($erro)){
+                echo $erro;
+            };
+
+            // esse erro serve ara alguma coisa
+        
+        ?>
+        <br>
+        <button type="submit">Entrar</button>
+    </form>
+
 </body>
 </html>
